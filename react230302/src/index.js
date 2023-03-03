35bb70e7d696f262390583af48536db52d55f09f
doo <ekseksgody@hanmail.net>
2023-03-03 17:28:11 +0900

    ✨ react 오후수업

--- react230302/src/index.js
+++ react230302/src/index.js
@@ -15,3 +15,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// console.log()가 두 번 뜨는 것은 StrictMode로 인한 정상적인 상황
+// 거슬린다면 index.js에서 출력할 파일을 감싸고 있는 <StrictMode></StrictMode>를 제거하면 된다.
